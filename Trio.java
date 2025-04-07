e603253c63e31ef74209fb5171d4a20797107427
bHammons07 <brody.hammons@stu.daviess.kyschools.us>
2025-04-07 14:27:22 +0000

    aaaaaaaaaaaaa

--- Trio.java
+++ Trio.java
@@ -1,1 +1,36 @@
 //Write your answer here
+public class Trio
+{
+    //Trio implements MenuItem̈́
+    public string name1;
+    public string name2;
+    public string name3;
+    public string theName;
+    public double price;
+
+    public static void main(String[] args)
+    {
+        public Trio(sandwich, salad, drink);
+        {
+            name1 = sandwich.getName();
+            name2 = salad.getName();
+            name3 = drink.getName();
+        }
+        
+        public String getName()
+        {
+            return theName;
+        }
+        public double getPrice()
+        {
+            return price;
+        }
+
+        public String toString()
+        {
+            return "" + name1 + "/" + name2 + " " + name3 + "Trio"
+        }
+        
+    }
+
+}
